12273e7eec6e2b0732a89c74abddc15c071bb876
tdnikolaeva <tdnikolaeva@mail.ru>
2025-07-01 16:35:31 +0300

    make MACHINE a command line argument

--- scripts/launch-vm.sh
+++ scripts/launch-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Dir is required argument. Example: $0 /path/to/poky"
+    echo "Dir is required argument. Example: $0 /path/to/poky machine."
     exit 1
 fi
 
@@ -12,55 +12,26 @@ if [ ! -d "$POKY_DIR" ]; then
     exit 1
 fi
 
-#так как build может содержать образы для нескольких машин
-#происходит поиск конкретных собранных образов
-#и предлагается выбор среди них
-select_machine() {
-    local poky_dir="$1"
-    local result_machine_name="$2"
-    local deploy_dir="$poky_dir/build/tmp/deploy/images"
-
-    if [ ! -d "$deploy_dir" ]; then
-        echo "Deploy directory [$deploy_dir] not found. Please build images first."
-        exit 1
-    fi
-
-    local machines=()
-    while IFS= read -r -d '' dir; do
-        machines+=("$(basename "$dir")")
-    done < <(find "$deploy_dir" -mindepth 1 -maxdepth 1 -type d -print0)
-
-    if [ ${#machines[@]} -eq 0 ]; then
-        echo "No built images found in $deploy_dir"
-        exit 1
-    fi
-
-    echo "Available machines:"
-    for i in "${!machines[@]}"; do
-        echo "  $((i+1))) ${machines[i]}"
-    done
-
-    local choice
-    while true; do
-        read -rp "Enter the number of the machine to run: " choice
-        if [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= ${#machines[@]} )); then
-            printf -v "$result_machine_name" '%s' "${machines[choice-1]}"
-            return 0
-        else
-            echo "Invalid choice, please enter a number between 1 and ${#machines[@]}"
-        fi
-    done
-}
-
-
-select_machine "$POKY_DIR" MACHINE
-
-if [ -z "$MACHINE" ]; then
-    echo "Can't get MACHINE"
+if [ -z "$2" ]; then
+    echo "Machine is required argument. Example: $0 /path/to/poky machine"
     exit 1
 fi
 
-echo "Use $MACHINE"
+MACHINE="$2"
+
+DEPLOY_DIR="$POKY_DIR/build/tmp/deploy/images"
+
+if [ ! -d "$DEPLOY_DIR" ]; then
+    echo "Deploy dir [$DEPLOY_DIR] not found."
+    echo "Build Yocto images first."
+    exit 1
+fi
+
+if [ ! -d "$DEPLOY_DIR/$MACHINE" ]; then
+    echo "Machine [$MACHINE] not found in $DEPLOY_DIR."
+    echo "Make sure that the machine is correct and that the image has been created."
+    exit 1
+fi
 
 source "$POKY_DIR/oe-init-build-env" "$POKY_DIR/build"
 
