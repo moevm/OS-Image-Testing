96d2333c6baeb830d8deb8ab7ffd78ea1d0f0686
tdnikolaeva <tdnikolaeva@mail.ru>
2025-10-12 16:16:40 +0300

    feat: update stressor in disk test

--- layers/meta-image-tests/recipes-tests/tests/files/tests/performance/disks/test-disks-stress-ng.sh
+++ layers/meta-image-tests/recipes-tests/tests/files/tests/performance/disks/test-disks-stress-ng.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-perf stat -e block:block_rq_issue,block:block_rq_complete,block:block_rq_insert,block:block_io_done,cycles,instructions,cpu-clock \
-  -- stress-ng --io 4 --timeout 60s --metrics-brief
+perf stat -e block:block_rq_issue,block:block_rq_complete,block:block_rq_insert,block:block_io_done,cpu-clock -- stress-ng --hdd 2 --hdd-bytes 100M --timeout 60s --metrics-brief --oom-avoid
 
