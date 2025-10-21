ff2653c319000877570b00dd8ffbce0655bce474
Artanias <43622365+Artanias@users.noreply.github.com>
2025-10-22 01:11:04 +0300

    refactor: increases ptest timeout.

--- scripts/run-qemu-test.sh
+++ scripts/run-qemu-test.sh
@@ -2,7 +2,7 @@
 
 screen -L -Logfile /tmp/results/screen.log -h 10000 -dmS qemu runqemu qemux86-64 slirp nographic && \
 timeout 120 bash -c '
-until grep -q "root@qemux86-64" /tmp/results/screen.log 2>/dev/null; do 
+until grep -q "root@qemux86-64" /tmp/results/screen.log 2>/dev/null; do
     sleep 5
     echo "Waiting for system to boot..."
 done'
@@ -10,13 +10,13 @@ done'
 if [ $? -eq 0 ]; then
     > /tmp/results/screen.log
     echo 'Running tests...'
-    screen -S qemu -X stuff 'ptest-runner\n' && \
-    timeout 600 bash -c '
+    screen -S qemu -X stuff 'ptest-runner -t 7200\n' && \
+    timeout 7200 bash -c '
     until grep -q "STOP: ptest-runner" /tmp/results/screen.log 2>/dev/null; do
-        sleep 5 
+        sleep 5
         echo "Tests running..."
     done'
-    
+
     echo 'Shutting down QEMU...'
     screen -S qemu -X stuff 'poweroff\n'
     sleep 10
