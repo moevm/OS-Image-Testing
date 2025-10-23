a00f45e09ba1f1ef1df9ad78c22266e2de917428
Artyom Semidolin <43622365+Artanias@users.noreply.github.com>
2025-10-23 16:59:15 +0300

    feat: adds stress-ng dummy handler. (#72)

---
+++ src/imgtests/exec/base_util.py
@@ -0,0 +1,49 @@
+from imgtests.exec.exec import ExecResult, SSHClient, run_command, which
+
+
+class BaseTestUtil:
+    """Base class for the test tools.
+
+    This class provides a common interface for the test tools and handles
+    basic initialization logic.
+
+    Attributes:
+        name (str): Name of the utility.
+        ssh_client (SSHClient | None): Client to the remote.
+        path (str | None): Path to the utility executable.
+    """
+
+    def __init__(self, name: str, ssh_client: SSHClient | None = None) -> None:
+        self.name = name
+        self.ssh_client = ssh_client
+        self.path = which(self.name, ssh_client)
+
+    def __call__(self, cmd: list[str] | None = None) -> ExecResult:
+        """Executes the utility with the provided command.
+
+        Args:
+            cmd (list[str] | None): Command arguments to pass to the utility.
+
+        Returns:
+            ExecResult: Result of the execution.
+        """
+        if cmd is None:
+            cmd = []
+        if self.path is None:
+            return ExecResult(stderr=f"Failed to locate '{self.name}'.", returncode=1)
+        if self.ssh_client is None:
+            return run_command([str(self.path), *cmd])
+        return self.ssh_client(" ".join([str(self.path), *cmd]))
+
+    def install(self) -> ExecResult:
+        """Installs the utility.
+
+        Raises:
+            NotImplementedError: If the installation logic is not implemented
+                for the specific utility.
+
+        Returns:
+            ExecResult: Execution result of the installation operation.
+        """
+        not_implemented_message = f"The '{self.name}' install logic is not implemented."
+        raise NotImplementedError(not_implemented_message)
