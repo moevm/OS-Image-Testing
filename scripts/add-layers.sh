1b76a6b535f0dd29a994676b88a60e49f87416ca
nhitar <nikita.pivoev@yandex.ru>
2025-10-25 14:45:54 +0300

    fix: return layers list for dependencies

--- scripts/add-layers.sh
+++ scripts/add-layers.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-mapfile -t LAYERS < <(find . -maxdepth 2 -type d -name "meta-*" | sort)
-
-# Layers without configuration files
-EXCLUDE_LAYERS=("meta-openembedded" "meta-secure-core")
+LAYERS=(
+    "meta-image-tests"
+    "meta-clang"
+    "meta-dpdk"
+    "meta-erlang"
+    "meta-openembedded/meta-oe"
+    "meta-openembedded/meta-python"
+    "meta-openembedded/meta-networking"
+    "meta-openembedded/meta-perl"
+    "meta-openembedded/meta-initramfs"
+    "meta-openembedded/meta-filesystems"
+    "meta-openembedded/meta-multimedia"
+    "meta-openembedded/meta-webserver"
+    "meta-openembedded/meta-gnome"
+    "meta-openembedded/meta-xfce"
+    "meta-qt5"
+    "meta-secure-core/meta-signing-key"
+    "meta-secure-core/meta-ids"
+    "meta-secure-core/meta-secure-core-common"
+    "meta-secure-core/meta-efi-secure-boot"
+    "meta-secure-core/meta-tpm2"
+    "meta-secure-core/meta-encrypted-storage"
+    "meta-secure-core/meta-integrity"
+    "meta-security"
+    "meta-virtualization"
+)
 
 for layer in "${LAYERS[@]}"; do
     layer="${layer#./}"
     layer_path="/home/user/poky/${layer}"
-
-    skip=false
-    for excluded in "${EXCLUDE_LAYERS[@]}"; do
-        if [ "$layer" = "$excluded" ]; then
-            echo "Skipping excluded layer: $layer"
-            skip=true
-            break
-        fi
-    done
-
-    if [ "$skip" = true ]; then
-        continue
-    fi
 
     if [ -d "$layer_path" ]; then
         echo "Adding layer: $layer"
