0b288f41ea4e1c89d8bd260a3ab959527e0c568d
Vadim Kazakov <vadimdim308@gmail.com>
2025-11-11 12:45:25 +0300

    Download tests results direclty from qemu vm, not from container

--- scripts/get-remote-results.py
+++ scripts/get-remote-results.py
@@ -1,9 +1,4 @@
-import os
-from typing import Final
 from imgtests.exec.exec import SSHClient
 
-PASSWORD: Final = os.environ.get("SSH_PASSWORD")
-USER: Final = os.environ.get("SSH_USER")
-
-ssh_client = SSHClient("10.5.0.10", USER, PASSWORD, 22)
-ssh_client.download(remotepath="/home/user/results_file", localpath="/home/user/results_file")
+ssh_client = SSHClient("10.5.0.10", root, "", 2222)
+ssh_client.download(remotepath="/root/results_file", localpath="/home/user/results_file")
