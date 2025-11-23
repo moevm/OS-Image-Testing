cdc4fae2b8923a4af78937ec5a10bbe5e23795fc
Artanias <43622365+Artanias@users.noreply.github.com>
2025-11-23 14:10:40 +0300

    fix: forword yocto container ports to the hosts.

--- scripts/cmd_yocto.sh
+++ scripts/cmd_yocto.sh
@@ -5,4 +5,5 @@ set -e
 cd "${POKY_DIR}"
 ./add-layers.sh
 bitbake "${OS_IMAGE}"
-runqemu qemux86-64 slirp nographic
+QB_SLIRP_OPT="-netdev user,id=net0,hostfwd=tcp::8080-:80,hostfwd=tcp::2222-:22" \
+    runqemu qemux86-64 slirp nographic
