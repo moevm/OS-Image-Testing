055ddcf6a3a2a6baa28cf35705d6d9fac2724420
Nikita Krinkin <45718678+NikitaKrinkin@users.noreply.github.com>
2025-11-27 00:08:13 +0300

    fix: open suse image cloud setup (#126)
    Co-authored-by: Artanias <43622365+Artanias@users.noreply.github.com>

---
+++ scripts/opensuse/cloud-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat << EOF > user-data
+#cloud-config
+password: $2
+chpasswd:
+  expire: false
+users:
+- default
+- name: $1
+  ssh_redirect_user: true
+  plain_text_passwd: $2
+  sudo: ['ALL=(ALL) NOPASSWD:ALL']
+  groups: users, sudo, admin
+  shell: /bin/bash
+  lock_passwd: false
+EOF
+
+cat << EOF > meta-data
+instance-id: 420/susevm
+EOF
+
+cloud-localds cloud-init.iso user-data meta-data
