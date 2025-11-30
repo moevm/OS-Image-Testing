e39e216618f6ab2d97a44275b71ce5269ff500ce
Pivoev Nikita <144446490+nhitar@users.noreply.github.com>
2025-11-30 12:26:45 +0300

    refactor: submodule initialization order and chown in entrypoint.sh (#134)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-sudo mkdir --parents "${BUILD_DIR}"/build "${BUILD_DIR}"/conf
-sudo mkdir --parents "${POKY_DIR}"/downloads
-sudo mkdir --parents "${POKY_DIR}"/sstate-cache
-sudo chown --recursive "${USER}":"${GROUP}" "${BUILD_DIR}" "${POKY_DIR}"/downloads "${POKY_DIR}"/sstate-cache
+sudo chown "${USER}":"${GROUP}" "${BUILD_DIR}"/conf
 
 sudo service ssh start
 
