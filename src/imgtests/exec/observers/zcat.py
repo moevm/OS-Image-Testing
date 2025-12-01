346286e090d9619bb74a936f093d5f491bfa4c74
Artanias <43622365+Artanias@users.noreply.github.com>
2025-12-01 16:52:04 +0300

    refactor: more generic way for getting contents from compressed files.

--- src/imgtests/exec/observers/zcat.py
+++ src/imgtests/exec/observers/zcat.py
@@ -1,3 +1,6 @@
+from collections.abc import Sequence
+from pathlib import Path
+
 from imgtests.exec.base_util import GenericUtil
 from imgtests.exec.exec import SSHClient
 
@@ -6,5 +9,5 @@ class Zcat(GenericUtil):
     def __init__(self, ssh_client: SSHClient | None = None) -> None:
         super().__init__("zcat", ssh_client)
 
-    def get_compressed_files_contents(self, files: list[str]) -> tuple[str, ...]:
-        return tuple(self(files).stdout.strip().split("\n"))
+    def get_compressed_files_contents(self, files: Sequence[str | Path]) -> tuple[str, ...]:
+        return tuple(self([str(file) for file in files]).stdout.strip().split("\n"))
