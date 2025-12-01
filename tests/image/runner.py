a80c21ea0d7ef7185b16c7e30426be594c0ff36a
Artanias <43622365+Artanias@users.noreply.github.com>
2025-12-01 17:45:14 +0300

    feat: also logs system info when starting tests.

--- tests/image/runner.py
+++ tests/image/runner.py
@@ -10,6 +10,7 @@ from image.utils import env_var_to_type_or_exit
 from imgtests.exec.exec import SSHClient
 from imgtests.exec.loaders.stress_ng import StressNg
 from imgtests.logger import set_handlers
+from imgtests.sysrep import get_system_info
 
 logger = logging.getLogger(__name__)
 set_handlers(logger, Path("processing.log"))
@@ -37,5 +38,6 @@ def wait_remote() -> SSHClient | None:
 if __name__ == "__main__":
     client = wait_remote()
     stress_ng = StressNg(client)
+    logger.info(get_system_info(client))
     _, metrics = stress_ng.run(timeout_sec=20, cpu=1)
     logger.info(metrics)
