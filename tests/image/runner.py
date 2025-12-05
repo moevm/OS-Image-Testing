1c13f989db354a084209dd6ed2c0853a8d4201d1
nhitar <nikita.pivoev@yandex.ru>
2025-12-06 01:33:47 +0300

    refactor: ruff hooks

--- tests/image/runner.py
+++ tests/image/runner.py
@@ -8,8 +8,8 @@ import paramiko.ssh_exception
 
 from image.utils import env_var_to_type_or_exit
 from imgtests.exec.exec import SSHClient
-from imgtests.exec.loaders.stress_ng import StressNg
 from imgtests.exec.loaders.pts import PhoronixTestSuite
+from imgtests.exec.loaders.stress_ng import StressNg
 from imgtests.logger import set_handlers
 from imgtests.sysrep import get_system_info
 
@@ -39,18 +39,17 @@ def wait_remote() -> SSHClient | None:
 if __name__ == "__main__":
     client = wait_remote()
 
-    pts = PhoronixTestSuite(client)
-    pts.setup()
-
     stress_ng = StressNg(client)
+    pts = PhoronixTestSuite(client)
 
     system_info = get_system_info(client)
     logger.info(system_info.tools_versions)
     logger.info(system_info.uname_info)
-    logger.info("Packages count %d", len(system_info.package_list))    
+    logger.info("Packages count %d", len(system_info.package_list))
 
     _, stress_ng_metrics = stress_ng.run(timeout_sec=20, cpu=1)
     logger.info(stress_ng_metrics)
 
+    pts.setup()
     PTS_metrics = pts.run(test_name="pts/pybench", run_count=2)
     logger.info(PTS_metrics)
