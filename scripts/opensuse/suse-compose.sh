6f8877557c23299d9751f2a861ab4816638668f6
NikitaKrinkin <nikita@krinkin.com>
2025-12-14 14:25:57 +0300

    sh fixes

--- scripts/opensuse/suse-compose.sh
+++ scripts/opensuse/suse-compose.sh
@@ -3,6 +3,6 @@
 set -e
 
 ./scripts/download-images.sh "$1"
-./scripts/cloud-setup.sh ${S_USER} ${PASSWORD}
+./scripts/cloud-setup.sh "${S_USER}" "${PASSWORD}"
 qemu-system-x86_64 -m 4G -nographic -drive file=open-suse-"$1".qcow2,index=0,media=disk \
-				   -cdrom cloud-init.iso -net user,hostfwd=tcp::${SSH_SUSE_PORT}-:22 -net nic
+				   -cdrom cloud-init.iso -net user,hostfwd=tcp::"${SSH_SUSE_PORT}"-:22 -net nic
