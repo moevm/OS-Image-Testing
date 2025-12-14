c0a8f9a3de86cddd8b2186b7819dabc4c62e3e0f
Nikita Krinkin <45718678+NikitaKrinkin@users.noreply.github.com>
2025-12-14 22:19:06 +0300

    feat: added suse to compose (#160)
    Closes #123.
    
    Co-authored-by: Artanias <43622365+Artanias@users.noreply.github.com>

--- scripts/opensuse/download-images.sh
+++ scripts/opensuse/download-images.sh
@@ -3,7 +3,7 @@
 SUSE_DIST_URL="https://download.opensuse.org/repositories/Cloud:/Images:/Leap_""$1""/images/"
 
 SUSE_15_5_IMG_FN="openSUSE-Leap-15.5.x86_64-1.0.1-NoCloud-Build6.180.qcow2"
-SUSE_15_6_IMG_FN="openSUSE-Leap-15.6.x86_64-1.0.2-NoCloud-Build1.86.qcow2"
+SUSE_15_6_IMG_FN="openSUSE-Leap-15.6.x86_64-1.0.2-NoCloud-Build1.93.qcow2"
 
 download () {
     # original file
