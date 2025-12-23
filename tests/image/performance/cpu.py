9a8194af7b6f0528a7f9ae4462f064983656f28f
tdnikolaeva <tdnikolaeva@mail.ru>
2025-12-23 15:00:12 +0300

    test: add test for chaosblade+perf stat

--- tests/image/performance/cpu.py
+++ tests/image/performance/cpu.py
@@ -2,6 +2,8 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 
 from imgtests.exec.exec import SSHClient
+from imgtests.exec.loaders.chaosblade import Chaosblade
+from imgtests.exec.loaders.perf import Perf
 from imgtests.exec.loaders.stress_ng import StressNg
 
 logger = logging.getLogger(__name__)
@@ -13,3 +15,26 @@ def run_stress_ng_tests(executor: ThreadPoolExecutor, client: SSHClient | None =
     result = future.result()
     _, metrics = result
     logger.info(metrics)
+
+
+def run_chaosblade_test(executor: ThreadPoolExecutor, client: SSHClient | None = None) -> None:
+    chaos = Chaosblade(client)
+    perf = Perf(client)
+    future_chaos = executor.submit(chaos.create_cpu_exp, cpu_percent=70, timeout_sec=15)
+    perf_result = perf.stat(
+        cmd=[
+            "-e",
+            "task-clock",
+            "-e",
+            "cpu-migrations",
+            "-e",
+            "cpu-clock",
+            "--",
+            "bash",
+            "-c",
+            "echo 'scale=2000; 4*a(1)' | bc -l",
+        ]
+    )
+    chaos_result = future_chaos.result()
+    logger.info(chaos_result)
+    logger.info(perf_result.stderr)
