0b7bcfda5ffad8bf526e0202af55bcb00f5dcc22
NikitaKrinkin <nikita@krinkin.com>
2025-12-23 14:34:24 +0300

    Bash fix

--- scripts/bencher/bencher-test.sh
+++ scripts/bencher/bencher-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl --tlsv1.2 -sSfL bencher_console:3000/download/install-cli.sh | sh
-source $HOME/.cargo/env
+source "$HOME"/.cargo/env
 
 bencher run --host http://bencher_api:61016 --project yocto-bencher-test "bencher mock"
