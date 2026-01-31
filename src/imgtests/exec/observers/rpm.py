cc90c02bac89ae6f5612f8bcc9c0b94d446af7fb
NikitaKrinkin <nikita@krinkin.com>
2026-01-31 06:27:57 +0300

    ruff fix

--- src/imgtests/exec/observers/rpm.py
+++ src/imgtests/exec/observers/rpm.py
@@ -7,4 +7,8 @@ class RPM(GenericUtil):
         super().__init__("rpm", ssh_client)
 
     def get_pkglist(self) -> tuple[str, ...]:
-        return tuple(self(["-qa", "--queryformat", "'%{NAME} %{VERSION}:%{RELEASE}\n'"]).stdout.strip().split("\n"))
+        return tuple(self([
+            "-qa",
+            "--queryformat",
+            "'%{NAME} %{VERSION}:%{RELEASE}\n'"
+        ]).stdout.strip().split("\n"))
