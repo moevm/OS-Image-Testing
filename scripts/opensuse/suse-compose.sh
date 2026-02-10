50afae10808f0e22de8dc88a135b66e573f1a090
NikitaKrinkin <nikita@krinkin.com>
2026-02-10 19:17:26 +0300

    suse ready

--- scripts/opensuse/suse-compose.sh
+++ scripts/opensuse/suse-compose.sh
@@ -4,5 +4,5 @@ set -e
 
 ./scripts/download_images.py "$1"
 ./scripts/cloud-setup.sh "${S_USER}" "${PASSWORD}"
-qemu-system-x86_64 -m 4G -nographic -drive file=open-suse-"$1".qcow2,index=0,media=disk \
+qemu-system-x86_64 -m 4G -nographic -drive file=open-suse-"$1".ready.qcow2,index=0,media=disk \
 				   -cdrom cloud-init.iso -net user,hostfwd=tcp::"${SSH_SUSE_PORT}"-:22,hostfwd=tcp::${SUSE_NE_PORT}-:9100 -net nic
