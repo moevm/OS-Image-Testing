c60ddec1044638f240de21a87fc7fe6beee27d62
Vadim Kazakov <vadimdim308@gmail.com>
2026-02-17 14:40:53 +0300

    feat: use classes from user_commands file instead of separate files for dd and rm

--- src/imgtests/exec/observers/dd.py
+++
@@ -1,7 +0,0 @@
-from imgtests.exec.base_util import GenericUtil
-from imgtests.exec.exec import SSHClient
-
-
-class Dd(GenericUtil):
-    def __init__(self, ssh_client: SSHClient | None = None) -> None:
-        super().__init__("dd", ssh_client)
