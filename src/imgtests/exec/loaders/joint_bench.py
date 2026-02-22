2c562c4bff96e8f9dec7e14a30d234bbffe204d1
tdnikolaeva <tdnikolaeva@mail.ru>
2026-02-22 22:56:28 +0300

    feat: add run method

--- src/imgtests/exec/loaders/joint_bench.py
+++ src/imgtests/exec/loaders/joint_bench.py
@@ -1,4 +1,5 @@
-from typing import Literal
+import logging
+from typing import Any, Literal, get_args
 
 from imgtests.exec.loaders.perf import Perf
 from imgtests.exec.loaders.pts import PhoronixTestSuite
@@ -48,9 +49,33 @@ Target = Literal[
     "network",
     "ipc",
     "syscall",
-    "system",  # or no????
+    "system",
 ]
 
 
 class JointBench:
-    pass
+    def __init__(self):
+        self.logger = logging.getLogger()
+        self.tools = {}
+        for tool_name, config in TOOLS_CONFIG.items():
+            self.tools[tool_name] = config["class"]()
+
+    def run(self, target: Target):
+        if target not in get_args(Target):
+            err_msg = f"Invalid action '{target}'. Expected one of {get_args(Target)}."
+            raise ValueError(err_msg)
+
+        for tool_name, config in TOOLS_CONFIG.items():
+            tool_instance = self.tools[tool_name]
+            tests = config["target"].get(target, [])
+
+            if tests:
+                for test in tests:
+                    run_method = getattr(tool_instance, config["run"])
+                    _, metrics = run_method(test)
+                    self.logger.log("Run '%s' test '%s'", tool_name, test)
+                    self.save(metrics)
+
+    def save(self, json_metrics: dict[str, Any]):
+        # log
+        pass
