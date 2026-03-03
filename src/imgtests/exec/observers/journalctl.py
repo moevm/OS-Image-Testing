17a04ed85e459f7e9b9e122f88928cdba7112a55
NikitaKrinkin <nikita@krinkin.com>
2026-03-03 17:02:21 +0300

    records cound

--- src/imgtests/exec/observers/journalctl.py
+++ src/imgtests/exec/observers/journalctl.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from imgtests.exec.exec import common_run_command
 from typing import TYPE_CHECKING, NamedTuple
 
 from imgtests.exec.base_util import GenericUtil
@@ -23,19 +25,25 @@ class Journalctl(GenericUtil):
         if upper_bound is None:
             upper_bound = lower_bound
 
-        records = self(["-b", "-p", f"{lower_bound}..{upper_bound}"])
+        records = common_run_command(
+            ("sudo", self.name, "-b", "-p", f"{lower_bound}..{upper_bound}"), self.ssh_client
+        ).stdout.split('\n')
 
         return JournalctlResult(len(records), records)
 
     def by_priority_higher(self, lower_bound: int) -> JournalctlResult:
         # journalctl -p a -> returns logs by priority from `a` and higher
         # a - int or str according to man page
-        records = self(["-b", "-p", f"{lower_bound}"])
+        records = common_run_command(
+            ("sudo", self.name, "-b", "-p", f"{lower_bound}"), self.ssh_client
+        ).stdout.split('\n')
 
         return JournalctlResult(len(records), records)
 
     def by_grep(self, target: str):
-        records = self(["-b", "-g", f"{target}"])
+        records = common_run_command(
+            ("sudo", self.name, "-b", "-g", f"{target}"), self.ssh_client
+        ).stdout.split('\n')
 
         return JournalctlResult(len(records), records)
 
@@ -44,3 +52,22 @@ class Journalctl(GenericUtil):
 
     def oom_records(self):
         return self.by_grep("OOM")
+
+    def records_per_second(self) -> float:
+        records = common_run_command(
+            ("sudo", self.name, "-b", "-o", "short-full"), self.ssh_client
+        ).stdout.split('\n')
+
+        start = " ".join([records[0].split()[1], records[0].split()[2]])
+        end = " ".join([records[-1].split()[1], records[-1].split()[2]])
+
+        time_format = "%Y-%m-%d %H:%M:%S"
+        start_time = datetime.strptime(start, time_format)
+        end_time = datetime.strptime(end, time_format)
+
+        delta = end_time - start_time
+        delta_secs = delta.total_seconds()
+
+        if delta_secs > 0:
+            return len(records) / delta_secs
+        return -1
