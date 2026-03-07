f64cbd81b8c58d1676e499f0234905b62f47c07a
nhitar <nikita.pivoev@yandex.ru>
2026-03-08 01:27:00 +0300

    refactor: separate wget test from stress-ng

--- tests/image/endurance/network.py
+++ tests/image/endurance/network.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Final
 
 from imgtests.exec.exec import SSHClient, common_run_command
 from imgtests.exec.loaders import StressNg
+from imgtests.runner import AbstractRunnableTimeLimitedTest
 from imgtests.suites.drive.stress_ng import StressNgTest
 
 if TYPE_CHECKING:
@@ -13,22 +14,6 @@ logger = logging.getLogger(__name__)
 
 _GOOGLE_URL: Final = shlex.quote("http://142.250.185.206/")
 _DNS_SERVER: Final = shlex.quote("8.8.8.8")
-
-
-def test_endurance_network(client: SSHClient | None) -> None:
-    if common_run_command(
-        ["echo", "nameserver", _DNS_SERVER, ">>", "/etc/resolv.conf"], client
-    ).returncode:
-        logger.error("NETWORK endurance test FAILED")
-        return
-    result = common_run_command(
-        ["wget", "--timeout=10", "--tries=1", _GOOGLE_URL],
-        client,
-    )
-    if result.returncode:
-        logger.error("NETWORK endurance test FAILED")
-        return
-    logger.info("NETWORK endurance test PASSED")
 
 
 class StressNgEnduranceNetworkTest(StressNgTest):
@@ -42,6 +27,27 @@ class StressNgEnduranceNetworkTest(StressNgTest):
     def _run(self, executor: ThreadPoolExecutor, client: SSHClient | None, timeout: int) -> None:
         stress_ng = StressNg(client)
         params = {"sock": 2, "sock_ops": 2}
-        self.run_test(stress_ng, executor, timeout, params)
+        self.run_test(stress_ng=stress_ng, executor=executor, timeout=timeout, **params)
 
-        test_endurance_network(client=client)
+
+class WgetEnduranceNetworkTest(AbstractRunnableTimeLimitedTest):
+    def __init__(self, timeout: int) -> None:
+        super().__init__("Load CPU 70% with chaosblade.", {"network"}, timeout)
+
+    def _run(self, executor: ThreadPoolExecutor, client: SSHClient | None, timeout: int) -> None:
+        def run_test():
+            if common_run_command(
+                ["echo", "nameserver", _DNS_SERVER, ">>", "/etc/resolv.conf"], client
+            ).returncode:
+                self.logger.error("NETWORK endurance test FAILED")
+                return
+            result = common_run_command(
+                ["wget", f"--timeout={timeout}", "--tries=1", _GOOGLE_URL],
+                client,
+            )
+            if result.returncode:
+                self.logger.error("NETWORK endurance test FAILED")
+                return
+            self.logger.info("NETWORK endurance test PASSED")
+        future = executor.submit(run_test)
+        future.result()
