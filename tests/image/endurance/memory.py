27811e5e04449b094787fe828d011455274bf830
Pivoev Nikita <144446490+nhitar@users.noreply.github.com>
2026-03-28 17:17:07 +0300

    feat: extend stress-ng vm tests (#277)
    Closes #263.
    
    ---------
    
    Co-authored-by: Artanias <43622365+Artanias@users.noreply.github.com>

--- tests/image/endurance/memory.py
+++ tests/image/endurance/memory.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from imgtests.exec.loaders import StressNg
 from imgtests.runner import Subsystem, TestResult
@@ -9,6 +9,15 @@ if TYPE_CHECKING:
     from concurrent.futures import ThreadPoolExecutor
 
     from imgtests.exec.exec import SSHClient
+
+tests: list[dict[str, Any]] = [
+    # General memory stress test with mmap() callings
+    {"vm": 4, "vm_bytes": "35%", "vm_populate": True, "mmap": 4, "mmap_bytes": "35%"},
+    # Memory bandwidth test
+    {"memrate": 0, "memrate_rd_mbs": 500, "memrate_wr_mbs": 1000},
+    # Memory mappings of various sizes and calls test
+    {"mmaptorture": 0, "mmaptorture_bytes": "70%"},
+]
 
 
 class StressNgEnduranceMemoryTest(StressNgTest):
@@ -23,10 +32,7 @@ class StressNgEnduranceMemoryTest(StressNgTest):
         self, executor: ThreadPoolExecutor, client: SSHClient | None, timeout: int
     ) -> Iterable[TestResult]:
         stress_ng = StressNg(client)
-        yield from self.run_test(
-            stress_ng=stress_ng,
-            executor=executor,
-            timeout=timeout,
-            vm=2,
-            vm_bytes="16M",
-        )
+        for params in tests:
+            yield from self.run_test(
+                stress_ng=stress_ng, executor=executor, timeout=timeout, **params
+            )
