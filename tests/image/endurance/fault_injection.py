e8ab7f63d6e9b8a1c936d9d796d333ea05ae0511
nhitar <nikita.pivoev@yandex.ru>
2026-03-31 18:11:28 +0300

    feat: extend suite scenarios

--- tests/image/endurance/fault_injection.py
+++ tests/image/endurance/fault_injection.py
@@ -17,27 +17,37 @@ class FaultInjectionEnduranceTest(AbstractRunnableTimeLimitedTest):
     def __init__(self, timeout: int, iterations: int = 4) -> None:
         super().__init__(
             "Endurance test with periodic fault injection.",
-            frozenset({Subsystem.SYSTEM}),
+            frozenset({Subsystem.FILE, Subsystem.SYSTEM}),
             timeout,
         )
         self.iterations = iterations
 
     def _run(
-        self, executor: ThreadPoolExecutor, client: SSHClient | None, timeout: int
+        self,
+        executor: ThreadPoolExecutor,  # noqa: ARG002
+        client: SSHClient | None,
+        timeout: int,
     ) -> Iterable[TestResult]:
         kirk = Kirk(client)
         available_suites = kirk.list_suites()
-        if "syscalls" not in available_suites:
-            self.logger.warning("'syscalls' suite not available for the image with LTP.")
-            return TestResult(status=TestStatus.SKIPPED)
-        
+        scenarios = ["syscalls", "dio", "fs"]
+        for suite in scenarios:
+            if suite not in available_suites:
+                self.logger.warning("'%s' suite not available for the image with LTP.", suite)
+                return TestResult(status=TestStatus.SKIPPED)
+
         random.seed(timeout)
-        fault_probabilities = [random.randint(0, 100) if i % 2 == 1 else 0 for i in range(self.iterations)]
+        fault_probabilities = [
+            random.randint(0, 100) if i % 2 == 1 else 0  # noqa: S311
+            for i in range(self.iterations)
+        ]
         time_per_test = (timeout // self.iterations) + 1
 
         for fault_probability in fault_probabilities:
             started_at = datetime.now(tz=ZoneInfo("UTC"))
-            result, metrics_path = kirk.run(scenarios=["syscalls"], timeout=time_per_test, fault_injection=fault_probability)
+            result, metrics_path = kirk.run(
+                scenarios=scenarios, timeout=time_per_test, fault_injection=fault_probability
+            )
 
             if metrics_path:
                 yield TestResult(
