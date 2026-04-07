bf16d911ca56a33f697a67d825270e3ead7f2a28
Vadim Kazakov <vadimdim308@gmail.com>
2026-04-07 16:40:27 +0300

    feat: remove unused function from stress_ng_general.py

--- tests/image/performance/stress_ng_general.py
+++ tests/image/performance/stress_ng_general.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 from typing import TYPE_CHECKING, Any
 
-from imgtests.exec.exec import common_run_command
 from imgtests.exec.loaders import StressNg
 from imgtests.runner import Subsystem, TestResult
 from imgtests.suites.general.stress_ng import StressNgTest
@@ -36,32 +35,6 @@ def combine_params(test_combination: list[dict[str, Any]]) -> dict[str, Any]:
     for params in test_combination:
         test_params.update(params)
     return test_params
-
-
-def tests_config(client: SSHClient | None):
-    nproc_result = common_run_command(["nproc"], client)
-    nproc = int(nproc_result.stdout)
-    net_param = int(nproc / 2) if nproc > 1 else 1
-    net = {
-        "sock": net_param,
-        "netdev": net_param,
-        "udp_flood": net_param,
-    }
-    if net not in tests:
-        tests.append(net)
-    ipc_param = int(nproc * 3 / 4) if nproc > 1 else 1
-    ipc = {
-        "mq": ipc_param,
-        "pipe": ipc_param,
-        "sem": ipc_param,
-        "shm": ipc_param,
-    }
-    syscall_param = int(nproc / 2) if nproc > 1 else 1
-    syscall = {"syscall": syscall_param}
-    if syscall not in tests:
-        tests.append(syscall)
-    if ipc not in tests:
-        tests.append(ipc)
 
 
 class StressNgConsecutiveLoadTest(StressNgTest):
