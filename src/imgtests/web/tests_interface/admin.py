0c5a93d24d2efb4032889d17dbafaf091cfa5a55
Vadim Kazakov <vadimdim308@gmail.com>
2026-05-12 17:10:57 +0300

    feat: store distro data in database

--- src/imgtests/web/tests_interface/admin.py
+++ src/imgtests/web/tests_interface/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Distribution
+
+
+@admin.register(Distribution)
+class DistributionAdmin(admin.ModelAdmin):
+    list_display = ("id", "display_name", "name", "version", "is_active", "order")
+    list_display_links = ("display_name",)
+    list_editable = ("order", "is_active")
+    list_filter = ("is_active", "name")
+    search_fields = ("display_name", "name", "description", "version")
+
+    fieldsets = (
+        (
+            "General information",
+            {"fields": ("name", "version", "display_name", "description")},
+        ),
+        ("Settings", {"fields": ("order", "is_active")}),
+    )
