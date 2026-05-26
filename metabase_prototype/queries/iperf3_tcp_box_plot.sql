60080fff09c2d0f7ff467e92935dbe9752894a5c
Чинаева Маргарита <144445296+Margarita-Chi@users.noreply.github.com>
2026-05-26 18:19:21 +0300

    add box plot for tcp

---
+++ metabase_prototype/queries/iperf3_tcp_box_plot.sql
@@ -0,0 +1,22 @@
+SELECT
+    (interval_item->'sum'->>'start')::float AS interval_start,
+    (interval_item->'sum'->>'bits_per_second')::float AS bits_per_second,
+    "configuration".os,
+    "configuration".core_info,
+    experiment.type,
+    experiment.started_at,
+    l.command,
+    experiment.experiment_id
+FROM loader AS l
+JOIN experiment ON l.experiment_id = experiment.experiment_id
+JOIN "configuration" ON experiment.config_id = "configuration".config_id
+CROSS JOIN LATERAL jsonb_array_elements(
+    (l.result::jsonb)->'client'->'intervals'
+) AS interval_item
+WHERE l.command LIKE '%iperf3%'
+  AND l.command NOT LIKE '%--udp%'
+  [[ AND {{os}} ]]
+  [[ AND {{core_info}} ]]
+  [[ AND {{type}} ]]
+  [[ AND {{date_range}} ]]
+ORDER BY interval_start
