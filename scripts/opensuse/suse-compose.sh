487dbe3a55bf3557bda490b2b2628ef31149ce03
Artanias <43622365+Artanias@users.noreply.github.com>
2026-06-23 00:37:34 +0300

    refactor: add CPUS configuration for VM CPU cores.
    - Add CPUS=4 configuration to .env.dist for Yocto and Suse VMs.
    - Update cmd_yocto.sh to pass -smp ${CPUS} to runqemu;
    - Update suse-compose.sh to use -smp "${CPUS}" in qemu command.

--- scripts/opensuse/suse-compose.sh
+++ scripts/opensuse/suse-compose.sh
@@ -4,5 +4,6 @@ set -e
 
 ./scripts/download_images.py "$1"
 ./scripts/cloud-setup.sh "${S_USER}" "${PASSWORD}"
-qemu-system-x86_64 -m "${VM_RAM}" -nographic -monitor tcp:0.0.0.0:4444,server,nowait -drive file=open-suse-"$1".ready.qcow2,index=0,media=disk \
+CPUS=4
+qemu-system-x86_64 -m "${VM_RAM}" -smp "${CPUS}" -nographic -monitor tcp:0.0.0.0:4444,server,nowait -drive file=open-suse-"$1".ready.qcow2,index=0,media=disk \
 				   -cdrom cloud-init.iso -net user,hostfwd=tcp::"${SSH_SUSE_PORT}"-:"${SSH_QEMU_PORT}",hostfwd=tcp::"${SUSE_NE_PORT}"-:"${DEFAULT_NE_PORT}",hostfwd=tcp:0.0.0.0:"${IPERF3_PORT}"-:"${IPERF3_PORT}",hostfwd=udp:0.0.0.0:"${IPERF3_PORT}"-:"${IPERF3_PORT}" -net nic
