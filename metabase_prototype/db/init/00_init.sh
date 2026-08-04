08db0f425574deb0de14312cb9fd233fa7d95aeb
NikitaKrinkin <nikita@krinkin.com>
2026-08-04 18:00:15 +0300

    metabase demo enhancement

--- metabase_prototype/db/init/00_init.sh
+++ metabase_prototype/db/init/00_init.sh
@@ -8,6 +8,7 @@ SELECT 'CREATE DATABASE ' || quote_ident('${MVP_RESULTS_DB}')
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${MVP_RESULTS_DB}')\gexec
 SQL
 
+psql --username "$POSTGRES_USER" --dbname="$MVP_RESULTS_DB" -f /docker-entrypoint-initdb.d/backup.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$MVP_RESULTS_DB" -f /docker-entrypoint-initdb.d/01_schema.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$MVP_RESULTS_DB" -f /docker-entrypoint-initdb.d/02_data.sql
 
